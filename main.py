1ee95ec152aa16e84a0ff4c1d87713f296492dce
AJAdrianos <aj.lotterboy@gmail.com>
2024-05-01 21:13:49 +0200

    Added cache and cpu classes

---
+++ main.py
@@ -0,0 +1,3 @@
+# create classes that mimic the functionalities of a CPU, cache, and memory
+# fetch and parse instructions from an input file
+# fetch and parse init values for the memory bus from a separate input file
